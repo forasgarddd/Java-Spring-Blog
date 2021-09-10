498bccb5260920e3e4590ef02b0a2791371821fb
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2021-09-11 01:04:38 +0300

    Files cleared after merging

--- src/main/java/com/springapp/springapp/config/MvcConfig.java
+++ src/main/java/com/springapp/springapp/config/MvcConfig.java
@@ -1,19 +1,15 @@
 package com.springapp.springapp.config;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-=======
 import org.springframework.context.annotation.Configuration;
->>>>>>> origin/master
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-<<<<<<< HEAD
     @Value("${upload.path}")
     private String uploadPath;
 
@@ -23,8 +19,6 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file://" + uploadPath + "/");
     }
 
-=======
->>>>>>> origin/master
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
